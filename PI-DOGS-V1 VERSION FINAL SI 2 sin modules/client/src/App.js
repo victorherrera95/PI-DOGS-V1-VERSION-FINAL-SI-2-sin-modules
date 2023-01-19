d64d56741f5e044a81edb554ae2b5b6b2afe5a0d
victorherrera95 <herreracontrerasva@gmail.com>
2023-01-19 13:57:26 -0300

    deploy

---
+++ PI-DOGS-V1 VERSION FINAL SI 2 sin modules/client/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import { Route, Routes,BrowserRouter } from 'react-router-dom'
+import Home from './routes/home';
+import DogDetail from './routes/dogDetails.js'
+import About from './routes/about.js'
+import CreateDog from './routes/createDog';
+import Landing from './routes/landing';
+import Page404 from './routes/page404';
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:3001/";
+
+function App() {
+
+  return (
+
+    <BrowserRouter>  
+    {/* aplica las funcionabilidades que necesitan las rutas ej redireccionamiento */}
+      <Routes>
+        {/* sirve para poder crear las rutas */}
+        <Route exact path='/' element={<Landing/>}/>
+        <Route exact path='/home' element={<Home/>}/>
+        <Route exact path='/dogs/:idRaza' element={<DogDetail/>}/>
+        <Route exact path='/about'element={<About />}/>
+        <Route exact path='/createDog'element={<CreateDog/>}/>
+        <Route path='*' element={<Page404/>}/>
+      </Routes>
+     
+    </BrowserRouter>
+    
+  );
+}
+
+export default App;
